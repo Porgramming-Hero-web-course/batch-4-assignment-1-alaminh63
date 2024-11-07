47a6084ea9bd08411503d4a4e1698d09772ad562
Al Fattaul Islam <alaminhasanpro@gmail.com>
2024-11-07 10:21:45 +0600

    Solved problem 7

--- src/problem-7.ts
+++ src/problem-7.ts
@@ -1,3 +1,24 @@
 {
   // Problem 7
+
+  class Car {
+    public make: string;
+    public model: string;
+    public year: number;
+
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+
+    getCarAge(): number {
+      const thePresentYear = new Date().getFullYear();
+      const printRemainingYear = thePresentYear - this.year;
+      return printRemainingYear;
+    }
+  }
+
+  const car = new Car('Honda', 'Civic', 2018);
+  car.getCarAge();
 }
